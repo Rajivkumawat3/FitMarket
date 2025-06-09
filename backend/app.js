c454c7e8c821b4fa7330f03549ba9b16a86f59f6
rajivkumawat3 <rajivkumawat28012002@gmail.com>
2025-06-09 18:02:18 +0530

    Complete user feature set

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const errorMiddleware = require("./middleware/error");
+const cookieParser=require("cookie-parser")
+const bodyParser=require("body-parser")
+const fileupload=require("express-fileupload")
+const dotenv=require("dotenv")
+const path=require("path");
+const cors = require('cors');
+
+   dotenv.config()
+
+
+   dotenv.config({path:"backend/config/config.env"})
+
+
+
+   app.use(
+      cors({
+        origin: [process.env.FRONT_END_URL],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+      })
+    );
+
+    app.use(cookieParser())
+    
+app.use(express.json())
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(fileupload())
+
+
+//Route imports
+
+const user=require("./routes/userRoute")
+
+
+
+app.use("/api/v1",user)
+
+// app.use(express.static(path.join(__dirname,"../frontend/build")))
+// app.get("*",(req,res)=>{
+//      res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
+// })
+// middleware for errors 
+app.use(errorMiddleware);
+
+module.exports = app;
