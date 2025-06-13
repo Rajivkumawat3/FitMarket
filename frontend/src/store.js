fbe7bed158d4ff1d5fa4f2581b407d3ae2986a44
rajivkumawat3 <rajivkumawat28012002@gmail.com>
2025-06-14 00:11:33 +0530

    fixed bugs

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,31 +1,31 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-// import { productsReducer ,  productDetailsReducer, newReviewReducer, newProductReducer, productReducer, productReviewsReducer, reviewReducer} from "./reducers/productReducer";
-// import { profileReducer, userReducer,forgotPasswordReducer, allUsersReducer, userDetailsReducer  } from "./reducers/userReducer";
-// import { cartReducer } from "./reducers/cartReducer";
-// import { allOrdersReducer, myOrdersReducer, newOrderReducer, orderDetailsReducer, orderReducer } from "./reducers/orderReducer";
+import { productsReducer ,  productDetailsReducer, newReviewReducer, newProductReducer, productReducer, productReviewsReducer, reviewReducer} from "./reducers/productReducer";
+import { profileReducer, userReducer,forgotPasswordReducer, allUsersReducer, userDetailsReducer  } from "./reducers/userReducer";
+import { cartReducer } from "./reducers/cartReducer";
+import { allOrdersReducer, myOrdersReducer, newOrderReducer, orderDetailsReducer, orderReducer } from "./reducers/orderReducer";
 
-// const reducer=combineReducers({
-//     products:productsReducer,
-//     productDetails: productDetailsReducer,
-//     user:userReducer,
-//     profile:profileReducer,
-//     forgotPassword :forgotPasswordReducer ,
-//     cart:cartReducer,
-//     newOrder: newOrderReducer,
-//     myOrders: myOrdersReducer,
-//     orderDetails: orderDetailsReducer,
-//     newReview: newReviewReducer,
-//     newProduct: newProductReducer,
-//     product:productReducer,
-//     allOrders: allOrdersReducer,
-//   order: orderReducer,
-//   allUsers: allUsersReducer,
-//   userDetails: userDetailsReducer,
-//   productReviews: productReviewsReducer,
-//   review: reviewReducer,
-// })
+const reducer=combineReducers({
+    products:productsReducer,
+    productDetails: productDetailsReducer,
+    user:userReducer,
+    profile:profileReducer,
+    forgotPassword :forgotPasswordReducer ,
+    cart:cartReducer,
+    newOrder: newOrderReducer,
+    myOrders: myOrdersReducer,
+    orderDetails: orderDetailsReducer,
+    newReview: newReviewReducer,
+    newProduct: newProductReducer,
+    product:productReducer,
+    allOrders: allOrdersReducer,
+  order: orderReducer,
+  allUsers: allUsersReducer,
+  userDetails: userDetailsReducer,
+  productReviews: productReviewsReducer,
+  review: reviewReducer,
+})
 
 let initialState={
     cart: {
@@ -44,7 +44,7 @@ let initialState={
 const middleware=[thunk]
 
 const store=createStore(
-    // reducer,
+    reducer,
     initialState,
     composeWithDevTools(applyMiddleware(...middleware))
 )
